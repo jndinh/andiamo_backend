8351f6dda80e6ff21a0a3eb2b4623cf09517cdce
Nicholas Lawrence <NGLawrence97@gmail.com>
2018-04-29 18:35:29 -0400

    Added login and register functionality

--- andiamo_backend/webservice/models.py
+++ andiamo_backend/webservice/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_id = models.IntegerField(primary_key=True)
+    user_id = models.AutoField(primary_key=True)
     email = models.CharField(max_length=40)
-    password = models.CharField(max_length=60)
+    password = models.CharField(max_length=128)
     fname = models.CharField(max_length=40)
     lname = models.CharField(max_length=40)
